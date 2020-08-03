2b3c3d47a196a6e7c4f5575c86950a6920481bc0
PhiPro95 <PProvenzano@web.de>
2020-08-03 21:01:31 +0200

    Update mc-fix_mc-170012 to v2.0.0

--- src/main/java/ocd/mc196725/mixin/ServerLightingProviderMixin.java
+++ src/main/java/ocd/mc196725/mixin/ServerLightingProviderMixin.java
@@ -1,17 +1,12 @@
 package ocd.mc196725.mixin;
 
-import java.util.concurrent.CompletableFuture;
-
 import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.server.world.ServerLightingProvider;
-import net.minecraft.util.Util;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.ChunkSectionPos;
 import net.minecraft.world.chunk.Chunk;
@@ -20,27 +15,15 @@ import ocd.mc196725.ILightUpdatesHandler;
 @Mixin(value = ServerLightingProvider.class, priority = 1001)
 public abstract class ServerLightingProviderMixin
 {
-    @Shadow
-    protected abstract void enqueue(int x, int z, ServerLightingProvider.Stage stage, Runnable task);
-
     @Dynamic(mixin = ocd.mc170012.mixin.ServerLightingProviderMixin.class)
     @Inject(
-        method = "setupLightmaps(Lnet/minecraft/class_2791;)Ljava/util/concurrent/CompletableFuture;",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/server/world/ServerLightingProvider;enqueue(IILnet/minecraft/server/world/ServerLightingProvider$Stage;Ljava/lang/Runnable;)V",
-            ordinal = 0,
-            shift = Shift.AFTER
-        )
+        method = "setupLightmaps(Lnet/minecraft/class_2791;)V",
+        at = @At("TAIL"),
+        remap = false
     )
-    private void enableLightUpdates(final Chunk chunk, final CallbackInfoReturnable<CompletableFuture<Chunk>> ci)
+    private void enableLightUpdates(final Chunk chunk, final CallbackInfo ci)
     {
         final ChunkPos pos = chunk.getPos();
-
-        this.enqueue(pos.x, pos.z, ServerLightingProvider.Stage.PRE_UPDATE, Util.debugRunnable(() -> {
-                ((ILightUpdatesHandler) this).enableLightUpdates(ChunkSectionPos.withZeroY(ChunkSectionPos.asLong(pos.x, 0, pos.z)));
-        },
-            () -> "enableLightUpdates " + pos
-        ));
+        ((ILightUpdatesHandler) this).enableLightUpdates(ChunkSectionPos.withZeroY(ChunkSectionPos.asLong(pos.x, 0, pos.z)));
     }
 }
